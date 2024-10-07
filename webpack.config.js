1da1255c267e3e5880398666a83259b90cbf7dcd
MiguelASFerreira <migant1306@gmail.com>
2024-10-07 15:17:04 -0300

    Instalando o Webpack Server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
     entry: path.resolve(__dirname, "src", "main.js"),
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    }
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        port: 3000,
+        open: true,
+        liveReload: true,
+    },
 }
