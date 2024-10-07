f0eb51747f97a1aeb1ae6175365d6d8810ff8680
MiguelASFerreira <migant1306@gmail.com>
2024-10-07 15:51:38 -0300

    Incluindo o Babel na aplicação

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,17 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
             }
         ]
     }
